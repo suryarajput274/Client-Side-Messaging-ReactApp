7b05175ec116f6ecb14acff07c0f446df959f2ce
Suraj <suryarajput274@gmail.com>
2020-09-13 15:37:22 +0530

    create Header

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import  React from 'react';
+import './Header.css';
+
+function Header() {
+
+    return(
+        <div className= "header">
+            <ul className="nav" >
+                <li>Home</li>
+                <li>Contact</li>
+                <li>About </li>
+
+            </ul>    
+        </div>   
+    );
+ }
+
+
+export default Header;
